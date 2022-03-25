5084693e7502f12626251292b2f9ec2f559ce15f
Kang Jiaming <kangjm2005@hotmail.com>
2022-03-25 10:25:27 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+purge: [
+    './index.html',
+    './src/**/*.{vue.js.ts.jsx.tsx}'
+     ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+    fontFamily:{
+        'HY45': ['HYRuanYuan', '45W']
+    }
+  extend: {},
+},
+variants: {},
+plugins: [],
+}
