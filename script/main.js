b5d675f092e7dc2d01a1e8f5aad7007451498dfc
Kang Jiaming <kangjm2005@hotmail.com>
2022-04-03 19:18:31 +0800

    adding user panel

--- script/main.js
+++ script/main.js
@@ -1,8 +1,6 @@
 import './index.css'
 import { Guard } from "@authing/native-js-ui-components";
 
-// 引入 css 文件
 import "@authing/native-js-ui-components/lib/index.min.css";
 
-// 你的 APP ID
 const guard = new Guard("62454142a50b99eb6086af65");
